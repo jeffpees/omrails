9d2f3b37f9656e5fa1dc3efbf4c6eed321e08a23
Jeff Pees <jeff.pees@gmail.com>
2013-03-18 22:16:14 -0700

    Add Image Upload by URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,14 +1,40 @@
+require 'open-uri'
+require 'timeout'
+
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+
+  belongs_to :user
+  attr_accessor :image_url
+  attr_accessible :description, :image, :image_url, :image_remote_url
+
+  before_validation :download_remote_image, :if => lambda { |pin| pin.image_url.present? }
+
+  has_attached_file :image, styles: { medium: "320x240>"}
 
   validates :description, presence: true
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
-  														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-  														 size: { less_than: 5.megabytes }
+  														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/img'] },
+  														 size: { less_than: 5.megabytes } 
 
+  private
   
+  def image_url_provided?
+    !self.image_url.blank?
+  end
 
-  belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240>"}
+  def download_remote_image
+    return if self.image.present?
+    	begin
+	      Timeout::timeout(2) do
+	        cat = open URI.parse(image_url)
+	        def cat.original_filename
+	        	base_uri.path.split('/').last.scan(/([\w\.]*\.(?:png|jpe?g|gif|bmp|JPG|JPEG|BMP|GIF|PNG))/).flatten.first
+	      	end
+	      self.image = cat.original_filename.blank? ? nil : cat
+  end
+    rescue Exception => e
+      Rails.logger.error "Failed to download image from \"#{image_url}\": #{e.message}"
+    end
+  end
 end
